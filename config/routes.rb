8cded5a0726f222964529e5dff5ad9522cf96ee0
Dzmitry42 <pr30dim@gmail.com>
2024-12-05 18:38:35 +0300

    i18

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :films
-  get "profile", to: "profiles#show"
-  devise_for :user
-  #         controllers: {
-  #omniauth_callbacks: 'users/omniauth_callbacks'
-  #}
+  scope "/:locale" do
+    resources :films
+    get "home/index"
+    root "home#index"
+    get "profile", to: "profiles#show"
+    devise_for :user
+               # controllers: {
+               #omniauth_callbacks: 'users/omniauth_callbacks'
+               #}
 
-  get "home/index"
 
-  root "home#index"
-  resources :films
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :films
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+    # Render dynamic PWA files from app/views/pwa/*
+    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+    # Defines the root path route ("/")
+    # root "posts#index"
+    end
 end
